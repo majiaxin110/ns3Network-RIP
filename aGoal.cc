6eccbae2021a759188519740adeaa00fa44ac424
majiaxin110 <majiaxin110@gmail.com>
2018-12-05 05:21:39 -0800

    first commit

---
+++ aGoal.cc
@@ -0,0 +1,18 @@
+#include <fstream>
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-apps-module.h"
+#include "ns3/ipv4-static-routing-helper.h"
+#include "ns3/ipv4-routing-table-entry.h"
+#include "ns3/netanim-module.h"
+#include "ns3/mobility-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("PoiRipRouting");
+
+int main (int argc, char **argv)
+{
+	return 0;
+}
